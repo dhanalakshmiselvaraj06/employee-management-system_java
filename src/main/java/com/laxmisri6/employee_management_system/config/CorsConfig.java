176e02d904cf2c579f7e281d5fb19e6d347d94e0
laxmisri6 <dhanalakshmi.sk612@gmail.com>
2026-06-01 02:10:41 +0200

    Fixed CORS for Vercel frontend

--- src/main/java/com/laxmisri6/employee_management_system/config/CorsConfig.java
+++ src/main/java/com/laxmisri6/employee_management_system/config/CorsConfig.java
@@ -15,10 +15,14 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.addAllowedOrigin("http://localhost:5173");
-        config.addAllowedOrigin("employee-management-frontend-react-6vptnm18q.vercel.app");
+
+        config.addAllowedOrigin(
+                "https://employee-management-frontend-react.vercel.app"
+        );
         config.addAllowedHeader("*");
 
         config.addAllowedMethod("*");
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
